c27608bdd8005ac4d4049a1ae9502e60ec5c6f34
Paul Mojica <paulmojicatech@gmail.com>
2021-08-08 07:57:03 -0400

    add toolbar

--- apps/fantalytic-admin-tool/src/app/app.module.ts
+++ apps/fantalytic-admin-tool/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,MatSidenavModule,MatToolbarModule,MatButtonModule,MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
