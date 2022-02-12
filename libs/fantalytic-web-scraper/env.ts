1bdb6d22722fd1802df29e31cba8965f1836c2d8
paulmojicatech <paulmojicatech@gmail.com>
2022-02-12 07:03:54 -0500

    Update env.ts

--- libs/fantalytic-web-scraper/env.ts
+++ libs/fantalytic-web-scraper/env.ts
@@ -1,2 +1,2 @@
 export const DB_NAME = 'fantalytic';
-export const MONGO_CONN = 'mongodb+srv://paulmojicatech:D00rb3ll@cluster0.qdjs6.azure.mongodb.net/fantalytic?retryWrites=true&w=majority';
+export const MONGO_CONN = '';
